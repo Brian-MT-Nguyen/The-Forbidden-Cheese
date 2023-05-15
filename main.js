8e9bea42e47be34dd31838f27920250237a2a741
Brian-MT-Nguyen <greencheezin@gmail.com>
2023-05-15 16:30:20 -0700

    added some skeleton files and folders (starter)

---
+++ main.js
@@ -0,0 +1,11 @@
+const game = new Phaser.Game({
+    scale: {
+        mode: Phaser.Scale.FIT,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+        width: 1920,
+        height: 1080
+    },
+    backgroundColor: 0x90ee90,
+    scene: [Demo],
+    title: "The Forbidden Cheese",
+});
---
+++ scenes/Demo.js
@@ -0,0 +1,5 @@
+class Demo extends Phaser.Scene {
+    constructor() {
+        super('demo');
+    }
+}
